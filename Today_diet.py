7431a0a5186158f134c61380a0a5796c59d2a0af
Hoan Lam <lamho@mail.gvsu.edu>
2022-12-29 11:07:09 -0500

    try to figure out the stuture of the diet menu

---
+++ Today_diet.py
@@ -0,0 +1,17 @@
+# Types of meals:       Protein             Carb            Fat
+# Mixed Meal            30%                 40%             30%
+# Paleo Meal            40%                 20%             40%
+# Low-carb Meal         40%                 10%             50%
+# Ketogenic Meal        20%                 5%              75%
+
+
+# Choose a diet for today
+option_string =""" 
+Types of meals:
+                        Protein             Carb            Fat
+Mixed Meal              30%                 40%             30%
+Paleo Meal              40%                 20%             40%
+Low-carb Meal           40%                 10%             50%
+Ketogenic Meal          20%                 5%              75%
+"""
+print(option_string)
